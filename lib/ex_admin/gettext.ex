c839d6720759c946771d52fb99d7f9814eabe09b
Daniel Parnell <me@danielparnell.com>
2017-11-05 15:27:51 -0800

    Get around the compiler error

--- lib/ex_admin/gettext.ex
+++ lib/ex_admin/gettext.ex
@@ -1,3 +1,25 @@
 defmodule ExAdmin.Gettext do
-  use Gettext, otp_app: :ex_admin
+  # use Gettext, otp_app: :ex_admin
+
+  def gettext(msgid) do
+    msgid
+  end
+
+  def gettext(msgid, bindings) do
+    msgid
+    |> String.split(~r/%\{[^\}]+\}/, include_captures: true)
+    |> Enum.map(fn s -> case Regex.run(~r/%\{([^\}]+)\}/, s) do
+                          nil -> s
+                          [_, v] -> bindings[String.to_atom(v)]
+                        end
+    end)
+    |> Enum.join()
+  end
+
+  def ngettext(msgid, msgid_plural, n, bindings \\ %{}) do
+    case n do
+      1 -> gettext(msgid, bindings)
+      _ -> gettext(msgid_plural, bindings)
+    end
+  end
 end
